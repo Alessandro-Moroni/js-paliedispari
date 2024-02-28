59dc9414f97ba4bfe39a827fa0d09df2ae14c795
Alessandro-Moroni <alemoro98@gmail.com>
2024-02-28 17:32:25 +0100

    [fix]parola-palindroma

--- js/script.js
+++ js/script.js
@@ -1,24 +1,16 @@
 let parolaInserita = prompt('Inserisci una parola palindroma');
 
-// const parolaUsata = parolaInserita.split('');
-// const parolaPalindroma = parolaUsata.reverse();
-// const parolaFinale = parolaPalindroma.join('');
 
-let parolaFinale = invertParola(parolaInserita);
-
-const risultatoParola = document.getElementById('word'); 
-
-if(parolaInserita == parolaFinale){
-  risultatoParola.innerHTML = `La tua parola <strong> ${parolaFinale} </strong> è palindroma!`;
-}else{
-  alert('Inserisci una parola palindroma');
-  risultatoParola.innerHTML = `La tua parola <strong> ${parolaInserita} </strong> NON è palindroma perchè il risultato è <strong> ${parolaFinale} </strong>!`;
+function parolaPalindroma(string){
+  for(let i = 0; i < string.length / 2; i++){
+    if(string[i] !== string[string.length -1 - i]){
+      return false;
+    }
+  }
+  return true;
 }
 
-function invertParola(string){
-  let stringInversa = string.split('').reverse().join('');
-  return stringInversa;
-}
+
 
 // Pari Dispari
 
@@ -44,12 +36,13 @@ if(verifica){
 
 const number = document.getElementById('number');
 let numeroScelto = parseInt(prompt('Inserisci un numero da 1 a 5'));
-let numeroComputer = (Math.ceil(Math.random() * 5));
-// let sommaNumeri = numeroScelto + numeroComputer;
-console.log(numeroComputer);
-console.log(sommaNumeri);
 
-function sommaNumeri(numeroScelto, numeroComputer){
+
+function pcNumber(min , max){
+  return min + Math.random() * (max - min);
+}
+
+
   
   if((numeroScelto + numeroComputer) % 2){
     number.innerHTML = `Il risultato è <strong> pari </strong>.`;
@@ -57,4 +50,3 @@ function sommaNumeri(numeroScelto, numeroComputer){
     number.innerHTML = `Il risultato è <strong> dispari </strong>.`;
   }
   return numeroScelto + numeroComputer;
-}
